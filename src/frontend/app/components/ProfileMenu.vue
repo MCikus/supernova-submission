071d2e5a39a46cdb02f52f1032cbe34b4f0e8f44
wasimTQ <wasimthoufiq21@gmail.com>
2022-06-10 12:44:58 +0530

    fix layout squeeze on edit mode

--- src/frontend/app/components/ProfileMenu.vue
+++ src/frontend/app/components/ProfileMenu.vue
@@ -13,19 +13,27 @@
         :closeMenu="closeMenu"
         :openMenu="openMenu"
       />
-      <div class="flex items-center px-3 mt-2 cursor-text hover:bg-gray-50">
-        <div v-if="showAvatar" class="w-8 h-8 rounded-full bg-blue-200 text-blue-800 text-xs flex justify-center items-center"><h5>TC</h5></div>
-        <InlineInput
-          class="cursor-pointer px-4 py-3 text-sm font-normal leading-none text-gray-700 flex justify-between"
+      <div class="mt-2 flex cursor-text items-center justify-center gap-4 px-5 hover:bg-gray-50">
+        <div
+          v-if="showAvatar"
+          class="flex h-8 w-8 items-center justify-center rounded-full bg-blue-200 text-xs text-blue-800"
+        >
+          <h5>TC</h5>
+        </div>
+        <div class="flex w-1/2 items-center gap-4">
+          <InlineInput
+          class="cursor-pointer text-sm font-normal leading-none text-gray-700"
           :value="userName"
           :multiline="false"
           :edit-by-inline-click="false"
           @inline-input-changed="userNameChanged"
         />
+        </div>
+        <!-- <input type="text"> -->
+        <!-- <h5>Frederick</h5>
+        <button>Edit</button> -->
       </div>
-       <slot
-        name="middle"
-      />
+      <slot name="middle" />
       <div class="w-full border-t border-gray-100" />
       <a
         href="/logout"
