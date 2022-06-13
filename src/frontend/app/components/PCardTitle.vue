7534e40c2e7bbc58152d22b3493d5b047e4eb211
yasmin <yasmin@yasminguimaraes.com>
2022-06-13 13:16:24 -0300

    1363 - card title component

---
+++ src/frontend/app/components/PCardTitle.vue
@@ -0,0 +1,13 @@
+<template>
+    <h3 class="text-lg leading-6 font-medium text-gray-900">Getting Started With Cryptocurrency</h3>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+export const componentName = 'PCardTitle'
+
+export default defineComponent({
+  name: componentName,
+  setup: () => ({ componentName }),
+})
+</script>
---
+++ src/frontend/app/components/PCardTopicPreviewComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- This example requires Tailwind CSS v2.0+ -->
+  <div class="bg-white">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="mt-6 space-y-4 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-4">
+        <div class="border border-gray-200 rounded-lg shadow-sm divide-y divide-gray-200">
+          <div class="p-6">
+            <PCardTitle></PCardTitle>
+            <p class="mt-4 text-sm text-gray-600">Brief description of this topic so that a user can get a first rough idea of what this topic is really about.</p>
+          </div>
+          <div class="pt-6 pb-8 px-6">
+            <h3 class="text-xs font-medium text-gray-900 tracking-wide uppercase">What's included</h3>
+            <ul role="list" class="mt-6 space-y-4">
+              <li class="flex space-x-3">
+                <!-- Heroicon name: solid/check -->
+                <svg class="flex-shrink-0 h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                </svg>
+                <span class="text-sm text-gray-500">Potenti felis, in cras at at ligula nunc.</span>
+              </li>
+
+              <li class="flex space-x-3">
+                <!-- Heroicon name: solid/check -->
+                <svg class="flex-shrink-0 h-5 w-5 text-green-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                </svg>
+                <span class="text-sm text-gray-500">Orci neque eget pellentesque.</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+export const componentName = 'PCardTopicPreviewComponent'
+import PCardTitle from '@/app/components/PCardTitle.vue'
+export default defineComponent({
+  name: componentName,
+  components: {
+    PCardTitle,
+  },
+  setup: () => ({ componentName }),
+})
+</script>
