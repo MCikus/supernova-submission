7fc1fc0c69d4acaf8c523003ff714af85c13d49a
Thomas Kral <mail@thomaskral.de>
2022-06-14 08:43:45 +0200

    Create CardListingColumns

---
+++ src/frontend/domain/cards/components/CardListingColumns.vue
@@ -0,0 +1,100 @@
+<template>
+    <Draggable
+      v-if="parents.length > 0"
+      class="flex flex-row"
+      :class="componentName"
+      :list="parents"
+      item-key="id"
+      v-bind="dragOptions"
+      :component-data="{
+        type: 'transition-group',
+        name: !drag ? 'transition-transform duration-500' : null,
+      }"
+      @start="drag = true"
+      @end="drag = false"
+    >
+      <template #item="{ element: parent }">
+        <div :key="parent.id" class="w-[350px]">
+          <div class="relative z-20 mx-2 h-[150px] bg-green-100 drop-shadow-md">
+            {{ parent.title }}
+          </div>
+          <div class="no-scrollbar relative z-10 h-full overflow-y-scroll pt-2">
+            <Draggable
+              v-if="parent.children.length > 0"
+              :list="children[parent.id]"
+              item-key="id"
+              v-bind="dragOptions"
+              :component-data="{
+                type: 'transition-group',
+                name: !drag ? 'transition-transform duration-500' : null,
+              }"
+              @start="drag = true"
+              @end="drag = false"
+            >
+              <template #item="{ element: child }">
+                <div
+                  :key="child.id"
+                  class="mx-2 mt-2 h-[150px] bg-blue-300 drop-shadow-md"
+                >
+                  {{ child.title }}
+                </div>
+              </template>
+              <template #footer>
+                <div class="flex w-full justify-center py-4">
+                  <button>AddButton</button>
+                </div>
+                <div class="h-[150px]"></div>
+              </template>
+            </Draggable>
+          </div>
+        </div>
+      </template>
+      <template #footer>
+        <div class="mx-2 flex items-start justify-start py-4">
+          <button>AddButton</button>
+        </div>
+      </template>
+    </Draggable>
+</template>
+
+<script>
+import { defineComponent, computed, onMounted, ref } from 'vue'
+import Draggable from 'vuedraggable'
+import { useCardsStore } from '@/domain/cards/services/useCardsStore.js'
+import { storeToRefs } from 'pinia'
+import { useRoute } from 'vue-router'
+
+export const componentName = 'CardListingColumn'
+
+export default defineComponent({
+  name: componentName,
+  components: {
+    Draggable,
+  },
+  setup() {
+    const titleCardId = computed(() => useRoute().params.titleCardId)
+    const useCards = useCardsStore()
+    const { parents, children } = storeToRefs(useCards)
+
+    const drag = ref(false)
+    const dragOptions = {
+      animation: 200,
+      disabled: false,
+      ghostClass: 'opacity-50',
+    }
+
+    onMounted(async () => {
+      await useCards.findAllParentsAndChildren(titleCardId.value)
+    })
+
+    return {
+      componentName,
+      titleCardId,
+      parents,
+      children,
+      drag,
+      dragOptions,
+    }
+  },
+})
+</script>
