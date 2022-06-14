c16a6a1bf42bda1bd81be09962b6e2a41a1cf0f2
Thomas Kral <mail@thomaskral.de>
2022-06-14 17:53:03 +0200

    Add BlockHandle

---
+++ src/frontend/domain/blocks/base/components/BlockHandle.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex flex-nowrap items-center h-fit text-[#5F75D7] mr-2">
+    <DotsVerticalIcon class="h-5 w-5 -mr-1.5"/>
+    <DotsVerticalIcon class="h-5 w-5 -ml-1.5"/>
+    <span class="bg-primary w-0.5 h-6 ml-0.5"></span>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { DotsVerticalIcon } from '@heroicons/vue/solid'
+
+export const componentName = 'BlockHandle'
+
+export default defineComponent({
+  name: componentName,
+  components: {
+    DotsVerticalIcon,
+  },
+  setup() {
+    return {
+      componentName,
+    }
+  },
+})
+</script>
+
+<style scoped></style>
