7f866714a91cc80ccff16f21fceadeede9087342
Wasim Thoufiq <wasimthoufiq21@gmail.com>
2022-06-14 13:44:49 +0530

    create new domain and subcomponents for notification

---
+++ src/frontend/domain/notifications/components/Notification.vue
@@ -0,0 +1,68 @@
+<template>
+  <div
+    aria-live="assertive"
+    class="inset-0 flex items-end pointer-events-none p-1 sm:items-start"
+  >
+    <div class="w-full flex flex-col items-center sm:items-end">
+      <div
+        class="
+          max-w-sm
+          w-full
+          bg-base-content
+          rounded-[4px]
+          pointer-events-auto
+          overflow-hidden
+          px-3
+          py-2
+        "
+      >
+        <div class="px-2 py-4">
+          <div class="flex gap-2 items-start">
+            <div class="flex-shrink-0">
+              <SunIcon class="h-6 w-6 text-accent" aria-hidden="true" />
+            </div>
+            <div class="flex-1">
+              <p class="text-sm text-base-100 max-w-[30ch]">{{ notification.message }}</p>
+              <button
+                v-if="notification.action?.text"
+                type="button"
+                class="
+                  mt-3
+                  rounded-md
+                  text-sm
+                  shadow-sm
+                  bg-accent
+                  text-base-100
+                  px-5
+                  py-2
+                  focus:outline-none
+                "
+                @click="notification.action?.action"
+              >
+                {{ notification.action?.text }}
+              </button>
+            </div>
+            <div class="flex-shrink-0 flex">
+              <button
+                type="button"
+                class="inline-flex text-base-100 hover:text-gray-500 focus:outline-none"
+              >
+                <span class="sr-only">Close</span>
+                <XIcon class="h-5 w-5" aria-hidden="true" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import { SunIcon } from '@heroicons/vue/outline'
+import { XIcon } from '@heroicons/vue/solid'
+import { NotificationProps } from '@/app/services/useNotificationsStore'
+
+const props = defineProps<{ notification: NotificationProps }>()
+</script>
---
+++ src/frontend/domain/notifications/components/NotificationStack.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="fixed max-w-[364px] w-full top-[72px] right-2 max-h-[80vh] z-50">
+    <Notification
+      v-for="notification in notifications"
+      :key="notification.id"
+      :notification="notification"
+    />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { storeToRefs } from 'pinia'
+import { useNotificationsStore } from '@/app/services/useNotificationsStore'
+import Notification from './Notification.vue'
+
+export const componentName = 'NotificationStack'
+
+export default defineComponent({
+  name: componentName,
+  components: { Notification },
+  setup() {
+    const notificationStore = useNotificationsStore()
+
+    const { notifications } = storeToRefs(notificationStore)
+
+    return {
+      notifications,
+    }
+  },
+})
+</script>
